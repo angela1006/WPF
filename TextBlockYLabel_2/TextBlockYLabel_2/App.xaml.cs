15c478ce9759421b69dbc41d166ff3171795b994
angela1006 <amormat1010@g.educaand.es>
2025-11-10 22:44:41 +0100

    añadir ejercicios de textblock y label

---
+++ TextBlockYLabel_2/TextBlockYLabel_2/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace WpfAppEjemplo
+{
+    public partial class MainWindow : Window
+    {
+        //arreglar -> no funciona
+        public MainWindow()
+        {
+            Grid mainGrid = new Grid();
+            Button myButton = new Button
+            {                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                FontWeight = FontWeights.Bold, 
+                Padding = new Thickness(10)      
+            };
+
+            WrapPanel myWrapPanel = new WrapPanel();
+
+            TextBlock tb1 = new TextBlock
+            {
+                Text = "Verde ",
+                Foreground = Brushes.Green, 
+                FontSize = 16
+            };
+
+            TextBlock tb2 = new TextBlock
+            {
+                Text = "Azul ",
+                Foreground = Brushes.Blue, 
+                FontSize = 16
+            };
+
+            TextBlock tb3 = new TextBlock
+            {
+                Text = "Sin formato",
+                FontSize = 16
+            };
+
+            myWrapPanel.Children.Add(tb1);
+            myWrapPanel.Children.Add(tb2);
+            myWrapPanel.Children.Add(tb3);
+
+            myButton.Content = myWrapPanel;
+
+            mainGrid.Children.Add(myButton);
+
+            this.Content = mainGrid;
+        }
+    }
+}
---
+++ TextBlockYLabel_3/TextBlockYLabel_3/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace TextBlockYLabel_3
+{
+    public partial class MainWindow : Window
+    {
+        // error de compilacioooon -> arreglar
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void MyButton_Click_Handler(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("1. ¡Haz click!\n";
+
+        }
+
+        private void StackPanel_Button_Click_Handler(object sender, RoutedEventArgs e)
+        {
+            string evento = "Button.Click ";
+            string comportamiento = "Este es un evento de tipo 'Bubbling' (burbujeo). " +
+                                    "Se originó en un boton hasta subir " +
+                                    "por el árbol hasta llegar a el StackPanel.";
+
+            MessageBox.Show($"capturado por el stackpanel!\n" +
+                            $"Evento manejado: {evento}\n" +
+                            $"Comportamiento: {comportamiento}\n" +
+                            $"Elemento que originó el evento: {e.OriginalSource}");
+        }
+    }
+}
