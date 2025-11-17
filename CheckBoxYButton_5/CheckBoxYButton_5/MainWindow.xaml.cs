693b3d7c2addfd9c656d2d651ae593cf13cb8d9a
angela1006 <amormat1010@g.educaand.es>
2025-11-17 22:33:52 +0100

    añadir ejercicios checkbox y button

---
+++ CheckBoxYButton_5/CheckBoxYButton_5/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace CheckBoxYButton_5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void todoChanged(object sender, RoutedEventArgs e)
+        {
+            bool? check = cbTodos.IsChecked;
+            cbM1.IsChecked = check;
+            cbM2.IsChecked = check;
+            cbM3.IsChecked = check;
+        }
+        private void indivChanged(object sender, RoutedEventArgs e)
+        {
+            int marcados = 0;
+
+            if (cbM1.IsChecked == true) marcados++;
+            if (cbM2.IsChecked == true) marcados++;
+            if (cbM3.IsChecked == true) marcados++;
+
+            if (marcados == 3)
+                cbTodos.IsChecked = true;
+            else if (marcados == 0)
+                cbTodos.IsChecked = false;
+            else
+                cbTodos.IsChecked = null; 
+        }
+    }
+}
---
+++ CheckBoxYButton_6/CheckBoxYButton_6/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxYButton_6
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+            txtBloque.Inlines.Clear(); 
+
+            if (chkAlternativo.IsChecked == true)
+            {
+                Run subrayado = new Run("Este texto está subrayado ")
+                {
+                    TextDecorations = TextDecorations.Underline
+                };
+                txtBloque.Inlines.Add(subrayado);
+                Span negrita = new Span(new Run("y esta palabra está en NEGRITA"))
+                {
+                    FontWeight = FontWeights.Bold
+                };
+                txtBloque.Inlines.Add(negrita);
+            }
+            else
+            {
+                txtBloque.Inlines.Add(new Run("Este es un texto simple sin formato."));
+            }
+        }
+    }
+}
